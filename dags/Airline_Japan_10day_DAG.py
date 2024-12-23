949730dcdd91dc7705968bb39391c2e823b9275c
juone00 <juwon0208@naver.com>
2024-12-24 01:45:14 +0900

    fix: GlueJobOperator 수정

--- dags/Airline_Japan_10day_DAG.py
+++ dags/Airline_Japan_10day_DAG.py
@@ -194,6 +194,7 @@ def transform(execution_time, extract_data):
     # AWS Glue 작업 실행
     try:
         glue_job = GlueJobOperator(
+            task_id='run_glue_job',
             job_name='team5-glue-test',  # Glue 작업 이름
             # script_location='s3://your-bucket/your-script.py',  # Glue 스크립트 경로
             # aws_conn_id='aws_default',  # AWS 연결 ID (Airflow 연결 설정에 맞게 수정)
