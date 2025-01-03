2df41d5ed71936f79007924b890a48a39e951546
NyHazzy <kimnayeon0525@gmail.com>
2025-01-03 16:06:06 +0900

    fix: glue iam 오류 수정

--- dags/flight_operations_DAG.py
+++ dags/flight_operations_DAG.py
@@ -463,7 +463,7 @@ with DAG(
             '--target_date': '{{ execution_date.in_timezone("Asia/Seoul").strftime("%Y%m%d") }}',
         },
         aws_conn_id='aws_default',
-        role_name='Team5-glue-role'
+        role_arn='arn:aws:iam::Team5:role/Team5-glue-role' 
     )
     
     # Snowflake 적재 태스크
