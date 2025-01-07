97bfd8cb73823bf8d84f7ea3a81f537bf89a1ccd
juone00 <juwon0208@naver.com>
2025-01-07 15:57:07 +0900

    test: 날짜 원상 복구

--- dags/utils/flight_etl.py
+++ dags/utils/flight_etl.py
@@ -30,7 +30,7 @@ def create_airport_task(airport_code, airport_name):
             execution_datetime = trans_to_kst(execution_time)
 
             # 날짜 계산 range(n) -> 앞으로 n일 계산
-            dates = [(execution_datetime + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(10)]
+            dates = [(execution_datetime + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(30)]
 
             tasks = []
             for date in dates:
